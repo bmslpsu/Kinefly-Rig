c14d4bf8660dd4aaa5bf85bff3ce8ed6544f97a2
boc5244 <31375763+boc5244@users.noreply.github.com>
2019-03-17 03:02:24 -0400

    Update Experiment_CL_HeadWing.m

--- Experiment_CL_HeadWing.m
+++ Experiment_CL_HeadWing.m
@@ -32,9 +32,9 @@ for kk = 1:n.trial
     WGain = Gain_all(kk,1);
 	HGain = Gain_all(kk,2);
 	filename = sprintf('%s_%i_%s_%i_%s_%i_%s_%i','fly',Fn,'trial',kk,'HGain',HGain,'WGain',WGain); % filename
-   	system(sprintf('export WGAIN=%f', WGain)) % set wing gain
-    system(sprintf('export HGAIN=%f', HGain)) % set head gain
-    [status,~] = system(['export LD_LIBRARY_PATH="LD_path";' ... % start Kinefly with set AO parameters
+	system(sprintf('export WGAIN=%f', WGain)) % set wing gain
+	system(sprintf('export HGAIN=%f', HGain)) % set head gain
+	[status,~] = system(['export LD_LIBRARY_PATH="LD_path";' ... % start Kinefly with set AO parameters
         'roslaunch Kinelfy main.launch' '& echo $!']);
     if status~=0
         error('Kinefly did not launch')
@@ -49,16 +49,16 @@ for kk = 1:n.trial
 	Panel_com('set_funcX_freq', 50); pause(n_pause)                 % 50Hz update rate for x-channel
     Panel_com('set_mode', [3,0]); pause(n_pause)                    % 0=open,1=closed,2=fgen,3=vmode,4=pmode
 	Panel_com('start') 	% start stimulus
-    pause(2)
+	pause(2)
 	Panel_com('stop')  	% stop stimulus
 	%-----------------------------------------------------------------------------------------------------------------------------
     % Closed-loop trial
     disp(['Closed-loop Trial ' num2str(kk)])
-    Panel_com('stop'); pause(n_pause)
-    Panel_com('set_pattern_id', 1);pause(n_pause)                   % set output to p_rest_pat (Pattern_Fourier_bar_barwidth=8)
-    Panel_com('set_position',[1, 4]); pause(n_pause)                % set starting position (xpos,ypos)
-    Panel_com('set_mode',[1,0]); pause(n_pause)                     % closed loop tracking (NOTE: 0=open, 1=closed)
-    Panel_com('send_gain_bias',[-10,0,0,0]); pause(n_pause)         % [xgain,xoffset,ygain,yoffset]
+	Panel_com('stop'); pause(n_pause)
+	Panel_com('set_pattern_id', 1);pause(n_pause)                   % set output to p_rest_pat (Pattern_Fourier_bar_barwidth=8)
+	Panel_com('set_position',[1, 4]); pause(n_pause)                % set starting position (xpos,ypos)
+	Panel_com('set_mode',[1,0]); pause(n_pause)                     % closed loop tracking (NOTE: 0=open, 1=closed)
+	Panel_com('send_gain_bias',[-10,0,0,0]); pause(n_pause)         % [xgain,xoffset,ygain,yoffset]
     [status,~] = system(['export LD_LIBRARY_PATH="LD_path";' ... % start recording
         'roslaunch Kinelfy record.launch prefix:=' filename ' & echo $!']);
     if status~=0
