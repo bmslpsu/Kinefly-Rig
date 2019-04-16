0ecca3b2b56e8ac4c70db4449673b376adf0ee84
Benjamin Cellini <31375763+boc5244@users.noreply.github.com>
2019-04-16 13:47:08 -0400

    Revised for Python 2 using rospy

--- Transform/mat2bag.py
+++ Transform/mat2bag.py
@@ -3,27 +3,24 @@ import glob, os
 import time, sys, math
 import numpy as np
 import h5py
-#from ros import rosbag
-#import roslib, rospy
-#from cv_bridge import CvBridge
+from ros import rosbag
+import roslib, rospy
+from cv_bridge import CvBridge
 import cv2
 from os import listdir
 ###################################################################################################
 dispFlag = False
 TOPIC = 'camera/image_raw' # topic
-root = 'Q:\Box Sync\Research\TEMP'
+root = '/media/jean-michel/Seagate Backup Plus Drive/0/Vid'
 os.chdir(root)
 for file in glob.glob("*.mat"):
-    filepath    = root + '\\' + file
+    filepath    = root + '/' + file
     filename    = os.path.splitext(os.path.basename(file))[0]
     bagname     = filename  + '.bag'       # output file .bag
-    bagpath     = root + '\\bag\\' + file
+    bagpath     = root + '/bag/' + bagname
 
     print('Converting:')
-    #print(filepath)
-    print(file)
-    #print(filename)
-    #print(bagname)
+    print(filepath)
 
     f = h5py.File(filepath,'r') # load file
     arrays  = {}
@@ -36,25 +33,31 @@ for file in glob.glob("*.mat"):
     nFrame  = vid.shape[0]          # # of frames
     tt      = arrays['t_v']         # time vector
 
-    Fs      = np.round(1/np.mean(np.diff(tt)),decimals=0,out=None) # frame rate
+    Fs      = np.round(1/np.mean(np.diff(tt)),decimals=0,out=None)/4 # frame rate
+    print('FRAME RATE:')
+    print(Fs)
 
-    #bag = rosbag.Bag(bagname, 'w')
-    #cb = CvBridge()
-
+    bag = rosbag.Bag(bagpath, 'w')
+    cb = CvBridge()
+    frame_id = 0
     for kk in range(nFrame):
-        frame   = vid[kk,:,:].T   # get frame
-        if dispFlag:
-            print(kk)
-            cv2.imshow('img',frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        #stamp              = rospy.rostime.Time.from_sec(float(kk) / Fs) # time stamp
-        #image              = cb.cv2_to_imgmsg(frameT, encoding='mono8')
-        #image.header.stamp = stamp
-        #image.header.kk    = "camera"
-        #bag.write(TOPIC, image, stamp)
+		frame   = vid[frame_id,:,:].T	# get frame
 
-    #bag.close()
+		if dispFlag:
+			print(frame_id)
+			cv2.imshow('img',frame)
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+
+		stamp 			= rospy.rostime.Time.from_sec(float(frame_id) / Fs)
+		image              	= cb.cv2_to_imgmsg(frame, encoding='mono8')
+		image.header.stamp 	= stamp
+		frame_id 			+=1
+
+		image.header.frame_id    = "camera"
+		bag.write(TOPIC, image,stamp)
+
+    bag.close()
 
     print('Done with:')
     print(bagpath)
